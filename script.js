c312506c5dc41ebaab1cf11547a5416f5ac163d4
HelioAugustoOliveira <00001105904982sp@al.educacao.sp.gov.br>
2024-10-27 17:02:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const hoursElement = document.querySelector("#hours")
+const minutesElement = document.querySelector("#minutes")
+const secondElement = document.querySelector("#seconds")
+
+function newTime() {
+
+const date = new Date();
+
+const hours = date.getHours()
+const minutes = date.getMinutes()
+const seconds = date.getSeconds()
+
+
+hoursElement.textContent = fixtame(hours)
+minutesElement.textContent = fixtame(minutes)
+secondElement.textContent = fixtame(seconds)
+
+function fixtame(time) {
+    return time < 10 ? "0"+time : time
+} 
+}
+newTime()
+setInterval(newTime, 1000)
